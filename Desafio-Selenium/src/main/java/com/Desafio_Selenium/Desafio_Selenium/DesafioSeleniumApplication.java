a0c004f0febc3241539b679dbcc5145f38a5230c
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2024-10-16 12:27:59 -0300

    create project

---
+++ Desafio-Selenium/src/main/java/com/Desafio_Selenium/Desafio_Selenium/DesafioSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.Desafio_Selenium.Desafio_Selenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSeleniumApplication.class, args);
+	}
+
+}
---
+++ Desafio-Selenium/src/test/java/com/Desafio_Selenium/Desafio_Selenium/DesafioSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desafio_Selenium.Desafio_Selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
